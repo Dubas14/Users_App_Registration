5060b6778254a1fe6bf977af295de29bb3c90fdc
User <User@EVHENIIHOME>
2024-03-28 11:46:27 +0200

    Добавьте файлы проекта.

---
+++ UsersApp/AuthWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Open_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Введіть коректні значення";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Введіть коректні значення";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+
+                User autUser = null;
+                using (ApplicationContext context = new ApplicationContext())
+                {
+                    autUser = context.Users.Where(b => b.Login == login && b.Pass == pass).FirstOrDefault();
+                }
+                if (autUser != null)
+                    MessageBox.Show("Ви війшли до кабінету");
+                else
+                {
+                    MessageBox.Show("Не коректні дані");
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ UsersApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    
+    {
+        ApplicationContext db;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new ApplicationContext();
+
+
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox2.Password.Trim();
+            string email = textBoxMail.Text.ToLower().Trim();
+
+            if(login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Введіть коректні значення";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Введіть коректні значення";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass != pass_2)
+            {
+                passBox2.ToolTip = "Паролі не співпадають";
+                passBox2.Background = Brushes.DarkRed;
+            }
+            else if (email.Length <5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxMail.ToolTip = "Паролі не співпадають";
+                textBoxMail.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox2.ToolTip = "";
+                passBox2.Background = Brushes.Transparent;
+                textBoxMail.ToolTip = "";
+                textBoxMail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Дякую ви зарееструвались!");
+                User user = new User(login, email, pass);
+
+
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+        }
+
+        private void Button_Open_Window_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            this.Hide();
+
+        }
+
+
+    }
+}
